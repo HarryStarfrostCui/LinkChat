e14345c2008ca5ef254a19ddda0d7005e42b457d
Harry <cuihy1999@gmail.com>
2020-02-08 11:39:56 -0800

    main activity interface rough set-up

--- app/src/main/java/com/example/linkchat/MainActivity.java
+++ app/src/main/java/com/example/linkchat/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.linkchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mAddChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAddChat = findViewById(R.id.main_add_chat);
+        mAddChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open add-class activity
+            }
+        });
     }
 }
