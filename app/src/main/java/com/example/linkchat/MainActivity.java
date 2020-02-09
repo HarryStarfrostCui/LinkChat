390e5357d86dce9a0646f9844a4793a3426ffe4f
Michael Chang <mchang0926@gmail.com>
2020-02-08 16:21:03 -0800

    Adding FireBase database library on both gradle and mainActivity

--- app/src/main/java/com/example/linkchat/MainActivity.java
+++ app/src/main/java/com/example/linkchat/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.linkchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
